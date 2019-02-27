f3426018d042bf020fed47c18abdd686248afb01
Waldemar <sh6943a@american.edu>
2019-02-27 01:30:52 -0500

    Tweaking

--- R/thesis.R
+++ R/thesis.R
@@ -17,7 +17,7 @@
 #' }
 dissertation_pdf <- function(toc = TRUE, toc_depth = 3, highlight = "default", ...){
 
-  base <- bookdown::pdf_book(template = "template.tex",
+  base <- bookdown::pdf_book(template = "sources/template.tex",
     toc = toc,
     toc_depth = toc_depth,
     highlight = highlight,
